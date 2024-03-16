ef70ee104c66ffda033f4f6d26fdb4d05bd37f5f
imdhemy <imdhemy@gmail.com>
2024-03-16 10:28:56 +0200

    chore(cicd): add psalm sca

--- src/Command.php
+++ src/Command.php
@@ -11,6 +11,9 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
 
+/**
+ * @psalm-api - This file is part of the symblaze/console package.
+ */
 abstract class Command extends SymfonyCommand
 {
     protected InputInterface $input;
