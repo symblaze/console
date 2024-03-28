ff6443b713a7e5fbba8a81105841233e6fd1b550
imdhemy <imdhemy@gmail.com>
2024-03-29 00:36:26 +0200

    chore: allow mocking output
    - for old symfony versions that don't type hint the `getFormatter()` method

--- src/IO/Output.php
+++ src/IO/Output.php
@@ -4,8 +4,8 @@ declare(strict_types=1);
 
 namespace Symblaze\Console\IO;
 
-use Symfony\Component\Console\Formatter\NullOutputFormatter;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\NullOutput;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
 
@@ -18,9 +18,10 @@ class Output extends SymfonyStyle
 {
     public function __construct(InputInterface $input, OutputInterface $output)
     {
-        $formatter = $output->getFormatter() ?? new NullOutputFormatter();
-        $output->setFormatter($formatter);
-        
+        if (is_null($output->getFormatter())) {
+            $output = new NullOutput();
+        }
+
         parent::__construct($input, $output);
     }
 }
